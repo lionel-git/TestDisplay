43734aa06c35dbf2b0ef4a55da42a58658c40418
lionel-git <lionel.desorme@free.fr>
2018-03-04 12:13:49 +0100

    Test2

---
+++ MyData/DataSummary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyData
+{
+    public class DataSummary
+    {
+        DateTime FileDate { get; set; }
+        string Name { get; set; }
+
+        public DataSummary()
+        {
+            FileDate = DateTime.Now;
+            Name = Guid.NewGuid().ToString();
+        }
+
+        public override string ToString()
+        {
+            return $" {Name} {FileDate}";
+        }
+
+    }
+}
---
+++ MyData/FileData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyData
+{
+    public class FileData
+    {
+        public DateTime FileDatetime { get; set; }
+        public IList<string> Names { get; set; }
+        public string Text { get; set; }
+
+        public FileData(string path)
+        {
+            Text=File.ReadAllText(path);
+        }
+    }
+
+
+}
